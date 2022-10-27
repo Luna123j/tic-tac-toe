0bbdadcb1c4e70d91db99c071605fe7768ba192d
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-10-27 19:56:47 -0400

    add Mark class

---
+++ library/src/tic_tac_toe/logic/models.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from enum import Enum
+
+
+class Mark(Enum):
+    CROSS = "X"
+    NAUGHT = "O"
+
+    @property
+    def other(self) -> "Mark":
+        return Mark.CROSS if self is Mark.NAUGHT else Mark.NAUGHT
+
+        
