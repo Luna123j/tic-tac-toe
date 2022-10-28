12dec7c2878073ffe4f4002ad7e24242071a5ee1
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-10-27 20:20:39 -0400

    modify validators

--- library/src/tic_tac_toe/logic/models.py
+++ library/src/tic_tac_toe/logic/models.py
@@ -4,6 +4,7 @@ from enum import Enum
 import re
 from dataclasses import dataclass
 from functools import cached_property
+from tic_tac_toe.logic.validators import validate_grid
 
 WINNING_PATTERNS = (
     "???......",
@@ -30,8 +31,7 @@ class Grid:
     cells: str = " " * 9
 
     def __post_init__(self) -> None:
-        if not re.match(r"^[\sXO]{9}$", self.cells):
-            raise ValueError("Must contain 9 cells of: X, O, or space")
+        validate_grid(self)
 
     @cached_property
     def x_count(self) -> int:
