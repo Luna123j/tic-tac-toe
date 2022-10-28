4530d4b7bb143d17d0b591d3c1abce53b6f6010b
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-10-27 20:12:45 -0400

    add more states and set winner parttern

--- library/src/tic_tac_toe/logic/models.py
+++ library/src/tic_tac_toe/logic/models.py
@@ -5,6 +5,16 @@ import re
 from dataclasses import dataclass
 from functools import cached_property
 
+WINNING_PATTERNS = (
+    "???......",
+    "...???...",
+    "......???",
+    "?..?..?..",
+    ".?..?..?.",
+    "..?..?..?",
+    "?...?...?",
+    "..?.?.?..",
+)
 
 class Mark(Enum):
     CROSS = "X"
@@ -35,6 +45,7 @@ class Grid:
     def empty_count(self) -> int:
         return self.cells.count(" ")
 
+
 @dataclass(frozen=True)
 class Move:
     mark: Mark
@@ -47,3 +58,30 @@ class Move:
 class GameState:
     grid: Grid
     starting_mark: Mark = Mark("X")
+
+    @cached_property
+    def current_mark(self) -> Mark:
+        if self.grid.x_count == self.grid.o_count:
+            return self.starting_mark
+        else:
+            return self.starting_mark.other
+
+    @cached_property
+    def game_not_started(self) -> bool:
+        return self.grid.empty_count == 9
+
+    @cached_property
+    def game_over(self) -> bool:
+        return self.winner is not None or self.tie
+
+    @cached_property
+    def tie(self) -> bool:
+        return self.winner is None and self.grid.empty_count == 0
+
+    @cached_property
+    def winner(self) -> Mark | None:
+        for pattern in WINNING_PATTERNS:
+            for mark in Mark:
+                if re.match(pattern.replace("?", mark), self.grid.cells):
+                    return mark
+        return None
